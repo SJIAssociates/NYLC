f091c5f659a56a3054b4bb2655d810ed4329b465
Collin Berg <berg.collin@gmail.com>
2019-08-28 15:54:06 -0400

    Added Mobile Menu Button

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,17 +5,23 @@
           <a class="brand" href="{{ home_url('/') }}">
             <img src="@asset('images/logo.png')">
           </a>
-        <nav class="nav-primary">
+        <nav class="hidden lg:block nav-primary">
           @if (has_nav_menu('primary_navigation'))
             {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
           @endif
         </nav>
       </div>
-      <div class='w-full sm:w-1/4 text-right'>
+      <div class='hidden lg:block  w-full md:w-1/4 text-right'>
         {!! get_search_form() !!}
         <a href="#" class='uppercase text-white text-sm font-bold'>Join Us</a>
         <a href="#" class='btn btn-donate'>Donate</a>
       </div>
+      <button id='mobile-menu' class='lg:hidden menu-toggle' type='button'>
+        <span class='bar top'></span>
+        <span class='bar middle'></span>
+        <span class='bar bottom'></span>
+        <span class='menu-title text-white'>Menu</span>
+      </button>
     </div>
   </div>
 </header>
