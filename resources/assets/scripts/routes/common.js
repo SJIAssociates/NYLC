cbe9742d3ee630306356bf38a00056a19140cb08
Collin B <berg.collin@gmail.com>
2019-08-28 21:51:43 -0400

    Mobile Menu JS for toggle

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,15 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    function menuMorph(){
+			$('.sitewrapper').toggleClass('openNav');
+			$('.middle').toggleClass('hidemiddle');
+			$('.bottom').toggleClass('rotateup');
+			$('.top').toggleClass('rotatedown');
+			$('.menu-toggle').toggleClass('collapse');
+			$('#mobile-nav').toggleClass('open');
+		}
+		$('.menu-toggle').click(menuMorph);	    
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
