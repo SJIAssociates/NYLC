2a1e81fa36804f56195ea9517e69fa60beea4830
Collin B <berg.collin@gmail.com>
2019-08-28 21:51:20 -0400

    Added Markup for Mobile Menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,20 +2,22 @@
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
   <body @php body_class() @endphp>
-    @php do_action('get_header') @endphp
-    @include('partials.header')
-    <div class="wrap" role="document">
-      
-        @yield('content')
-        
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
+    <div class="sitewrapper">
+      @php do_action('get_header') @endphp
+      @include('partials.header')
+      <div class="wrap" role="document">
 
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
+          @yield('content')
+
+          @if (App\display_sidebar())
+            <aside class="sidebar">
+              @include('partials.sidebar')
+            </aside>
+          @endif
+
+      @php do_action('get_footer') @endphp
+      @include('partials.footer')
+      @php wp_footer() @endphp
+  </div>
   </body>
 </html>
