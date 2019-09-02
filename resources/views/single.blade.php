95584255bcc4dca381f16fa86f1873d21e5f8c56
Collin B <berg.collin@gmail.com>
2019-09-02 18:58:50 -0400

    Fixed News page CSS

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -6,12 +6,15 @@
 <div class='container'>
   <div class="content">
     <div class='flex'>
-      <main class="main py-8 xl:w-3/4 xxl:py-12">  
+      <aside class='aside py-8 text-center w-1/4'>
+        Share News
+      </aside>
+      <main class="main py-8 xl:w-3/4 xxl:py-12">
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-single-'.get_post_type())
   @endwhile
-  </main> 
+  </main>
     </div>
-  </div>  
-</div>    
+  </div>
+</div>
 @endsection
