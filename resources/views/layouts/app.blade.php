10c0320981c7ff9792e6f8ceff9d459f74d9b3b2
Collin Berg <berg.collin@gmail.com>
2019-09-03 16:53:23 -0400

    Removed Sidebar from App and put it in Single

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,6 @@
 
           @yield('content')
 
-          @if (App\display_sidebar())
-            <aside class="sidebar">
-              @include('partials.sidebar')
-            </aside>
-          @endif
-
       @php do_action('get_footer') @endphp
       @include('partials.footer')
       @php wp_footer() @endphp
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -5,10 +5,12 @@
 @include('partials.page-header')
 <div class='container'>
   <div class="content">
-    <div class='flex'>
-      <aside class='aside py-8 text-center w-1/4'>
-        Share News
-      </aside>
+    <div class='flex xxl:w-3/4 mx-auto'>
+      @if (App\display_sidebar())
+        <aside class="sidebar py-8 w-1/4 pr-5">
+          @include('partials.sidebar')
+        </aside>
+      @endif
       <main class="main py-8 xl:w-3/4 xxl:py-12">
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-single-'.get_post_type())
