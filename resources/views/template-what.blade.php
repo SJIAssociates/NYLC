e1f29c1c6809889a7903e0628a19a5a24e72e2fd
Collin Berg <berg.collin@gmail.com>
2019-09-06 17:06:51 -0400

    Removed header

--- resources/views/template-what.blade.php
+++ resources/views/template-what.blade.php
@@ -17,13 +17,22 @@
   <section class='full-section' id="homeWhat">
   	<div class='container'>
       @include('partials/trio')
-      @foreach($success_stories_loop as $item)
-        @include('partials/box-success', $item)
-      @endforeach
-
-      <a href="/success_stories" class='underline text-black mx-auto block w-1/2 text-center'>View all Success Stories <i class='fa fa-chevron-right'></i></a>
   	</div>
   </section>
+<section class='full-section'>
+  <div class='container'>
+    <div class="text-center">
+		  <h2><span>Success Stories</span></h2>
+    </div>
+    <p class='font-bold text-center w-3/4 lg:w-3/5 mx-auto lg:text-2xl'>
+      With your support, we’re proud to celebrate a history of accomplishments since our founding in 1973.
+    </p>
+    @foreach($success_stories_loop as $item)
+      @include('partials/box-success', $item)
+    @endforeach
 
+    <a href="/success_stories" class='underline text-black mx-auto block w-1/2 text-center'>View all Success Stories <i class='fa fa-chevron-right'></i></a>
+  </div>
+</section>
 
 @endsection
