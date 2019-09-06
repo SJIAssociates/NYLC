d4d4e45078e1776d5b879ba2676e43a55d1bc734
Collin Berg <berg.collin@gmail.com>
2019-09-06 12:17:09 -0400

    Fixed the Top Trio To be better respsonive (WOP)

--- resources/views/partials/trio.blade.php
+++ resources/views/partials/trio.blade.php
@@ -1,17 +1,17 @@
 <div class='flex my-10 flex-wrap'>
-    <div class='w-full sm:w-1/3 text-center'>
+    <div class='w-full lg:w-1/3 text-center flex flex-wrap justify-center'>
       <h3 class='text-3xxl'>Advocacy</h3>
-      <p class='text-grey-darker w-full mx-auto my-8 text-xl lg:px-20'>Giving preservation needs a powerful voice and fighting for sensible development.</p>
+      <p class='text-grey-darker w-full mx-auto my-8 text-xl xl:px-12'>Giving preservation needs a powerful voice and fighting for sensible development.</p>
       <a href="" class='btn text-white  border-red inline-block font-bold w-auto'>Learn about our Advocacy</a>
     </div>
-    <div class='w-full sm:w-1/3 text-center'>
+    <div class='w-full lg:w-1/3 text-center flex flex-wrap justify-center'>
       <h3 class='text-3xxl'>Grants & Loans</h3>
-      <p class='text-grey-darker w-full mx-auto my-8 text-xl lg:px-20'>Saving landmarked buildings, revitalizing communities, and supporting local jobs.</p>
+      <p class='text-grey-darker w-full mx-auto my-8 text-xl xl:px-12'>Saving landmarked buildings, revitalizing communities, and supporting local jobs.</p>
       <a href="" class='btn text-white border-red inline-block font-bold w-auto'>Learn about Grants & loans</a>
     </div>
-    <div class='w-full sm:w-1/3 text-center'>
+    <div class='w-full lg:w-1/3 text-center flex flex-wrap justify-center'>
       <h3 class='text-3xxl'>Preservation Assistance</h3>
-      <p class='text-grey-darker w-full mx-auto my-8 text-xl lg:px-20'>Providing expert technical services to residential, commercial and religious building owners.</p>
-      <a href="" class='btn text-white border-red inline-block font-bold w-auto'>Get Help</a>
+      <p class='text-grey-darker w-full mx-auto my-8 text-xl xl:px-12'>Providing expert technical services to residential, commercial and religious building owners.</p>
+      <a href="" class='btn text-white border-red inline-block font-bold w-3/4'>Get Help</a>
     </div>
 </div>
--- resources/views/template-what.blade.php
+++ resources/views/template-what.blade.php
@@ -16,16 +16,12 @@
   @endwhile
   <section class='full-section' id="homeWhat">
   	<div class='container'>
-  		<div class='text-center'><h2><span>What We Do</span></h2></div>
-  		<p class='text-center font-bold xxl:mb-10'>Our work enriches the quality of life for all New Yorkers.</p>
-
       @include('partials/trio')
-
       @foreach($success_stories_loop as $item)
         @include('partials/box-success', $item)
       @endforeach
 
-      <a href="/success_stories" class='text-center text-underline text-black'>View all Success Stories</a>
+      <a href="/success_stories" class='underline text-black mx-auto block w-1/2 text-center'>View all Success Stories <i class='fa fa-chevron-right'></i></a>
   	</div>
   </section>
 
