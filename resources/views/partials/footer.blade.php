957baf0b9a36cd7c139c2a76d2dc6114d295a9a9
Collin Berg <berg.collin@gmail.com>
2019-09-13 16:33:34 -0400

    More Footer Changes for the CC form and responsive

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,12 +2,12 @@
 	<div class='container'>
 		<div class='text-center'><h2><span class='text-white'>Support Us</span></h2></div>
 		<div class='flex justify-center flex-wrap'>
-  		  <div class='card w-full p-5 bg-white text-center my-5 md:mx-5 md:w-1/3 md:p-10'>
+  		  <div class='card w-full p-5 bg-white text-center my-5 lg:mx-5 lg:w-1/3 md:p-10'>
     		  <h3 class='text-black text-4xl'>Donate</h3>
     		  <p class='font-grey my-5 m-auto md:w-3/4 block text-xl text-grey-darker '>Your donation helps us expand our reach and be your voice for preservation.</p>
     		  <a href="/donate" title="Make a Donation" class='btn border-red inline-block md:w-1/2'>Make A Donation</a>
   		  </div>
-  		  <div class='card w-full p-5 bg-white text-center my-5 md:mx-5 md:w-1/3 md:p-10'>
+  		  <div class='card w-full p-5 bg-white text-center my-5 lg:mx-5 lg:w-1/3 md:p-10'>
     		  <h3 class='text-black text-4xl'>Become A Member</h3>
     		  <p class='font-grey my-5 m-auto md:w-3/4 block text-xl text-grey-darker'>Join the Conservancy and be part of our mission to save New York’s extraordinary architectural heritage.</p>
     		  <a href="/donate" title="Make a Donation" class='btn border-red inline-block md:w-1/2 '>Join Us</a>
@@ -19,7 +19,7 @@
 	<div class='container'>
 		<div class='w-full md:w-4/5 mx-auto'>
 	  	<h2 class='text-white text-4xl font-bold xl:mb-5'>Join Our Mailing List</h2>
-	  	<p class='text-white w-1/2 font-bold'>Sign up to receive our free E-Newsletter, informative alerts, our monthly Mystery Landmark contest, and our monthly Tourist in Your Own Town video series.</p>
+	  	<p class='text-white w-1/2 font-bold text-lg mb-5'>Sign up to receive our free E-Newsletter, informative alerts, our monthly Mystery Landmark contest, and our monthly Tourist in Your Own Town video series.</p>
 	  	{!! $footer_form !!}
 		</div>
   </div>
@@ -53,9 +53,9 @@
 			  <p class='text-sm'>&copy; @php echo date("Y") @endphp {{ get_bloginfo('name', 'display') }}. All Rights Reserved.</p>
 		  </div>
 		  <nav class='md:w-1/2 text-right text-sm py-2'>
-			  <a href="#" class='text-grey-darkest pr-1 py-1 hover:text-red'>Terms of Service</a>
-			  <a href="#" class='text-grey-darkest pr-1 py-1 hover:text-red'>Privacy Policy</a>
-			  <a href="#" class='text-grey-darkest pr-1 py-1 hover:text-red'>Sitemap</a>
+			  <a href="#" class='text-grey-darkest pr-3 py-1 hover:text-red'>Terms of Service</a>
+			  <a href="#" class='text-grey-darkest pr-3 py-1 hover:text-red'>Privacy Policy</a>
+			  <a href="#" class='text-grey-darkest pr-3 py-1 hover:text-red'>Sitemap</a>
 		  </nav>
 	  </div>
   </div>
