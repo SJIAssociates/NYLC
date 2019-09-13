5817ab36c1682426788873ca0693cd53e41e2320
Collin Berg <berg.collin@gmail.com>
2019-09-13 16:34:17 -0400

    updated the view if there is no Featured Image

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -6,18 +6,20 @@
 <div class='container'>
 <div class="content">
   @if( $grant_sidebar != false)
-  <aside class='sidebar'>
+  <aside class='sidebar py-8 lg:w-1/4 xxl:py-12'>
     @foreach($grant_sidebar as $item )
-
-      <div class='widget'>
+      <div class='grant-sidebar'>
         <h3>{!! $item['title'] !!}</h3>
         {!! $item['content'] !!}
       </div>
-
     @endforeach
   </aside>
   @endif
-  <main class="main py-8 xxl:w-3/4 xxl:py-12 xxl:mx-auto">
+  @if( $grant_sidebar != false)
+  <main class="main py-8 xl:w-3/4 xxl:py-12 xxl:mx-auto">
+  @else
+  <main class="main py-8 lg:w-3/4 xxl:py-12 ">
+  @endif
       @while(have_posts()) @php the_post() @endphp
         @include('partials.content-page')
       @endwhile
