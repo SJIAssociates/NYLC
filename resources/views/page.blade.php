6b1c0850271a2cf1346663c2523795d6f64a54aa
Collin B <berg.collin@gmail.com>
2019-09-13 07:47:41 -0400

    Update page.blade.php
    Grant Sidebar

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,15 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-      
+
 @include('partials.page-header')
 <div class='container'>
 <div class="content">
-  <main class="main py-8 xxl:w-3/4 xxl:py-12 xxl:mx-auto">      
+  @if( $grant_sidebar != false)
+  <aside class='sidebar'>
+    @foreach($grant_sidebar as $item )
+
+      <div class='widget'>
+        <h3>{!! $item['title'] !!}</h3>
+        {!! $item['content'] !!}
+      </div>
+
+    @endforeach
+  </aside>
+  @endif
+  <main class="main py-8 xxl:w-3/4 xxl:py-12 xxl:mx-auto">
       @while(have_posts()) @php the_post() @endphp
         @include('partials.content-page')
       @endwhile
   </main>
-</div>  
-</div>    
+</div>
+</div>
 @endsection
