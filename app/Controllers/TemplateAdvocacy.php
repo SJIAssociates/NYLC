036352a5131678ebd796770cfb19ce52fb2b7ecb
Collin Berg <berg.collin@gmail.com>
2019-09-16 17:27:44 -0400

    Added the Advocacy Continued view

--- app/Controllers/TemplateAdvocacy.php
+++ app/Controllers/TemplateAdvocacy.php
@@ -20,10 +20,32 @@ class TemplateAdvocacy extends Controller
             'time'      => get_the_time('F d, Y', $post->ID),
             'permalink' => get_permalink( $post->ID ),
             'title'     => get_the_title( $post->ID ),
-            'thumbnail' => get_the_post_thumbnail($post->ID),
+            'thumbnail' => get_the_post_thumbnail_url($post->ID) ?: \App\asset_path('images/profile-placeholder.jpg'),
         ];
     }, $news_loop);
   }
 
+  public function advocacyLoopContinued(){
+
+    $news_loop_continue = get_posts([
+      'post_type' => 'post',
+      'category_name' => 'advocacy',
+      'posts_per_page' => 4,
+      'offset'         => 3,
+    ]);
+
+   return array_map(function ($post) {
+
+        return [
+            'content'   => get_the_excerpt( $post->ID ),
+            'time'      => get_the_time('F d, Y', $post->ID),
+            'permalink' => get_permalink( $post->ID ),
+            'title'     => get_the_title( $post->ID ),
+            'thumbnail' => get_the_post_thumbnail_url($post->ID) ?: \App\asset_path('images/profile-placeholder.jpg'),
+        ];
+    }, $news_loop_continue);
+
+
+  }
 
 }
--- resources/views/template-advocacy.blade.php
+++ resources/views/template-advocacy.blade.php
@@ -15,15 +15,33 @@
       </div>
     </div>
   @endwhile
-  <section class='full-section' id='homeNews'>
+  <section class='full-section bg-grey-light' id=''>
   	<div class='container'>
-  		<div class='text-center'><h2><span class='bg-grey'>Advocacy News</span></h2></div>
+  		<div class='text-center'><h2><span class='bg-grey-light'>Advocacy News</span></h2></div>
   		<p class='text-black text-xxl text-center font-semibold my-3 xxl:mb-10'>Stay informed about the latest preservation issues.</p>
   		<div class='flex my-8 flex-wrap'>
         @foreach($advocacy_loop as $item)
           @include('partials/box-news', $item)
         @endforeach
   		</div>
+      <div class='flex flex-wrap'>
+        @foreach($advocacy_loop_continued as $item)
+          <div class='flex px-5 mb-10 w-full lg:w-1/2'>
+            <div class='w-4/5'>
+              <time class='text-red font-bold uppercase text-sm mb-3 block'>{!! $item['time'] !!}</time>
+              <h3 class='text-lg xxl:text-2xl font-bold'><a href="{!! $item['permalink'] !!}" class='text-black'>{!! $item['title'] !!}</a></h3>
+              <p>{!! $item['content'] !!}</p>
+            </div>
+            <div class='w-1/5'>
+              <img src="{!! $item['thumbnail'] !!}" />
+            </div>
+          </div>
+
+        @endforeach
+      </div>
+      <div class='archive-link text-center'>
+        <a href="/category/advocacy" class='underline text-black'>View All Updates <i class='fa fa-chevron-right'></i></a>
+      </div>
   	</div>
   </section>
 
