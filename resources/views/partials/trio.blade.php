69281b82e4d48c3b30defac00340589a7b4b0eca
Collin Berg <berg.collin@gmail.com>
2019-10-04 12:42:38 -0400

    CSS changes

--- resources/views/partials/trio.blade.php
+++ resources/views/partials/trio.blade.php
@@ -1,5 +1,5 @@
     <div class='w-full lg:w-1/3 text-center items-stretch flex flex-wrap flex-col mb-10'>
       <h3 class='text-3xl'>{!! $title !!}</h3>
-      <p class='text-grey-darker w-full mx-auto flex-grow {!! $classes !!}'>{!! $description !!}</p>
+      <p class='text-black w-full mx-auto flex-grow {!! $classes !!}'>{!! $description !!}</p>
       <a href="{!! $link !!}" class='btn text-white  border-red inline-block font-bold w-11/12 xl:w-3/4 items-end mx-auto'>{!! $buttonText !!}</a>
     </div>
--- resources/views/template-advocacy.blade.php
+++ resources/views/template-advocacy.blade.php
@@ -10,15 +10,15 @@
   @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
     <div class='container'>
-      <div class='w-full md:w-2/3 xxl:w-1/2 mx-auto mb-10 xl:text-2xl'>
+      <div class='w-full md:w-2/3 mx-auto my-10 text-xl xl:text-2xl'>
         @include('partials.content-page')
       </div>
     </div>
   @endwhile
-  <section class='full-section bg-grey-light' id=''>
+  <section class='full-section bg-blue-grey' id=''>
   	<div class='container'>
-  		<div class='text-center'><h2><span class='bg-grey-light'>Advocacy News</span></h2></div>
-  		<p class='text-black text-xxl text-center font-semibold my-3 xxl:mb-10'>Stay informed about the latest preservation issues.</p>
+  		<div class='text-center'><h2><span class='bg-blue-grey'>Advocacy Updates</span></h2></div>
+  		<p class='text-black text-xxl text-center font-semibold my-3 xxl:mb-10'>Stay informed about the latest issues.</p>
   		<div class='flex my-8 flex-wrap'>
         @foreach($advocacy_loop as $item)
           @include('partials/box-news', $item)
@@ -39,8 +39,8 @@
 
         @endforeach
       </div>
-      <div class='archive-link text-center'>
-        <a href="/category/advocacy" class='underline text-black'>View All Updates <i class='fa fa-chevron-right'></i></a>
+      <div class=' text-center'>
+        <a href="/category/advocacy" class='archive-link underline text-black w-1/2 uppercase font-bold hover:text-primary'>View All Updates <i class='fa fa-chevron-right'></i></a>
       </div>
   	</div>
   </section>
