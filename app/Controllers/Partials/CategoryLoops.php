1384b0f5138cb2d67fe56cb3b44f75c2664acf14
Collin Berg <berg.collin@gmail.com>
2019-10-04 12:43:00 -0400

    Fixed Category loop returning null info

--- app/Controllers/Partials/CategoryLoops.php
+++ app/Controllers/Partials/CategoryLoops.php
@@ -7,27 +7,33 @@ trait CategoryLoops
   {
     $category = get_field('category');
 
+    if( $category ):
+
     $news_posts = get_posts([
       'post_type' => 'post',
       'category_name' => $category->name,
       'posts_per_page' => 3
     ]);
-
-   return array_map(function ($post) {
-         return [
-             'content'   => get_the_excerpt( $post->ID ),
-             'time'      => get_the_time('F d, Y', $post->ID),
-             'permalink' => get_permalink( $post->ID ),
-             'title'     => get_the_title( $post->ID ),
-             'thumbnail' => get_the_post_thumbnail_url($post->ID) ?: \App\asset_path('images/profile-placeholder.jpg'),
-         ];
-     }, $news_posts);
+     return array_map(function ($post) {
+          return [
+              'content'   => get_the_excerpt( $post->ID ),
+              'time'      => get_the_time('F d, Y', $post->ID),
+              'permalink' => get_permalink( $post->ID ),
+              'title'     => get_the_title( $post->ID ),
+              'thumbnail' => get_the_post_thumbnail_url($post->ID) ?: \App\asset_path('images/profile-placeholder.jpg'),
+          ];
+      }, $news_posts);
+    else:
+      return false;
+    endif;
   }
 
   public function newsLoopContinued()
   {
     $category = get_field('category');
 
+    if( $category ):
+    
     $news_loop_continue = get_posts([
       'post_type' => 'post',
       'category_name' => $category->name,
@@ -35,26 +41,31 @@ trait CategoryLoops
       'offset'         => 3,
     ]);
 
-   return array_map(function ($post) {
-
-        return [
-            'content'   => get_the_excerpt( $post->ID ),
-            'time'      => get_the_time('F d, Y', $post->ID),
-            'permalink' => get_permalink( $post->ID ),
-            'title'     => get_the_title( $post->ID ),
-            'thumbnail' => get_the_post_thumbnail_url($post->ID) ?: \App\asset_path('images/profile-placeholder.jpg'),
-        ];
-    }, $news_loop_continue);
-
+     return array_map(function ($post) {
+          return [
+              'content'   => get_the_excerpt( $post->ID ),
+              'time'      => get_the_time('F d, Y', $post->ID),
+              'permalink' => get_permalink( $post->ID ),
+              'title'     => get_the_title( $post->ID ),
+              'thumbnail' => get_the_post_thumbnail_url($post->ID) ?: \App\asset_path('images/profile-placeholder.jpg'),
+          ];
+      }, $news_loop_continue);
+    else:
+      return false;
+    endif;
   }
 
   public function categoryTitle()
   {
     $category = get_field('category');
 
-    return (object) array(
-          'name' => $category->name,
-          'link' => $category->slug,
-    );
+    if( $category ):
+      return (object) array(
+            'name' => $category->name,
+            'link' => $category->slug,
+      );
+    else:
+      return false;
+    endif;
   }
 }
