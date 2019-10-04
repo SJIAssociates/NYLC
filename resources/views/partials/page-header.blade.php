3afd8fd81db6f09a65cf15b807d44755ba23b96e
Collin Berg <berg.collin@gmail.com>
2019-10-04 12:44:08 -0400

    Updated Nav for responsive widths

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,6 +1,6 @@
 @if ( $header_image != false && !is_single() )
 <section class='img-bg flex justify-center items-center w-full' style="background-color: #ddd; background-image: url({{ $header_image }}); background-size: cover; background-position: 50% 50%;">
-  <h1 class='text-center text-white xxl:text-5xl xxl:w-1/2'>{!! App::title() !!}</h1>
+  <h1 class='text-center text-white text-4xl xl:text-5xl xxl:w-1/2'>{!! App::title() !!}</h1>
 </section>
 <div class="container">
 {!! $breadcrumbs !!}
