09d2aef112e9f36b91850cba44ae1f9b039d8f6b
Collin Berg <berg.collin@gmail.com>
2019-10-09 17:32:01 -0400

    styled 404 page more

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -2,11 +2,23 @@
 
 @section('content')
   @include('partials.page-header')
+  <section class='sub_pages full-section bg-blue-grey'>
+    <div class='container'>
+      <div class='flex flex-wrap'>
 
   @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+    <div class="alert alert-warning text-3xl text-center mx-auto w-full lg:w-2/3">
+
+      {{ __('Sorry, but the page you were trying to view does not exist. Please check that the page URL is spelled correctly.', 'sage') }}
     </div>
-    {!! get_search_form(false) !!}
+
+
   @endif
+    </div>
+    <div class='text-center my-10'>
+      <a href="/" class='btn mx-auto my-5 mr-5'>Go Home</a>
+      <a href="/" class='btn mx-auto my-5'>Contact Us</a>
+    </div>
+  </div>
+</section>
 @endsection
