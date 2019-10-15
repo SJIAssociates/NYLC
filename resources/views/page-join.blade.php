fe3c8fc44447155ee5518622272006ad0657b93e
Collin Berg <berg.collin@gmail.com>
2019-10-15 09:54:50 -0400

    responsive css for join

--- resources/views/page-join.blade.php
+++ resources/views/page-join.blade.php
@@ -35,20 +35,20 @@
 </section>
 <section class='full-section bg-blue-grey'>
   <div class='container'>
-      <div class="text-center">
+      <div class="text-center lg:mb-10">
         <h2><span class='bg-grey-lightest'>Membership Levels And Benefits</span></h2>
       </div>
-      <div class='flex flex-wrap'>
+      <div class='flex flex-wrap xxl:mx-auto xxl:w-5/6'>
         @foreach($membership_options as $item)
         <div class='membership-wrap w-full bg-white flex flex-wrap my-10'>
           <div class='w-full lg:w-1/4'>
             <img src="{{ $item['image'] }}" alt="{{ $item['title'] }}" class='w-full'>
           </div>
           <div class='w-full lg:w-1/2 p-5'>
-            <h3 class='text-3xl text-black'>{{ $item['title'] }}</h3>
+            <h3 class='text-2xl lg:text-3xl xx:text-4xl text-black'>{{ $item['title'] }}</h3>
             {!! $item['description'] !!}
           </div>
-          <div class='w-full lg:w-1/4 text-right '>
+          <div class='w-full lg:w-1/4 text-center lg:text-right '>
             <a href="{{ $item['page'] }}" class='btn btn-large text-white inline-block mr-5'>Join/Renew</a>
           </div>
         </div>
