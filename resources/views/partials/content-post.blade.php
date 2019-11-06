6d67e15e67f1b9e3d70bc4d022c1512d97e1336c
Collin Berg <berg.collin@gmail.com>
2019-11-06 17:33:39 -0500

    Missed this one

--- resources/views/partials/content-post.blade.php
+++ resources/views/partials/content-post.blade.php
@@ -1,7 +1,7 @@
 
-  <div class='news-card w-full bg-white mb-5 lg:mb-10 items-stretch relative p-12 lg:p-16 xxl:p-20'>
+  <div class='news-card w-full bg-white mb-5 lg:mb-10 items-stretch relative lg:p-12 lg:p-16 xxl:p-20'>
     <div class='flex flex-wrap'>
-        <div class='box-content w-full md:w-1/2 flex flex-wrap flex-col md:pr-8'>
+        <div class='box-content w-full md:w-1/2 flex py-3 px-6 lg:px-0 flex-wrap flex-col lg:pr-8'>
           <h3 class='text-2xl mb-1 xxl:text-3xl'>
             <a href="{!! get_the_permalink(); !!}" class='text-black hover:text-red'>{!! the_title();  !!}</a>
           </h3>
