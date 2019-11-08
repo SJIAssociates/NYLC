93afd4bb5e0ff6789506ae77c6b51849abf39b58
Collin Berg <berg.collin@gmail.com>
2019-11-08 16:33:21 -0500

    Styled 0 search results page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,10 +6,20 @@
     <div class="content">
       <div class='flex flex-wrap xxl:w-3/4 mx-auto'>
   @if (!have_posts())
-    <div class="alert alert-warning mx-auto lg:w-3/4">
+    <div class="alert alert-warning mx-auto lg:w-3/4 text-center">
       {{ __('Sorry, no results were found.', 'sage') }}
     </div>
-    {!! get_search_form(false) !!}
+
+    <form role="search" method="get" class="search-form flex my-1 mx-auto w-full lg:w-3/4 my-10" action="{{ esc_url( home_url( '/' ) ) }}">
+      <label class='w-3/4 bg-grey'>
+        <span class="screen-reader-text">{{ _x( 'Search for:', 'label' ) }}</span>
+        <input type="search" class="search-field bg-grey" placeholder="{!! esc_attr_x( 'Search &hellip;', 'placeholder' ) !!}" value="{{ get_search_query() }}" name="s" />
+      </label>
+      <button type="submit" class="search-submit" value="{{ esc_attr_x( 'Search ', 'submit button' ) }}" />
+        Search <i class='fas fa-search'></i>
+      </button>
+    </form>
+
   @endif
 
   @while(have_posts()) @php the_post() @endphp
