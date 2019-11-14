eb4afa40c42a8039b4f5d9ee9caf82aa403b39d2
Collin Berg <berg.collin@gmail.com>
2019-11-14 13:25:00 -0500

    Fixed success Stories Sidebar

--- resources/views/single-success_stories.blade.php
+++ resources/views/single-success_stories.blade.php
@@ -5,13 +5,13 @@
 @include('partials.page-header')
 <div class='container'>
   <div class="content">
-    <div class='flex xxl:w-3/4 mx-auto'>
+    <div class='flex flex-wrap xxl:w-3/4 mx-auto'>
       @if (App\display_sidebar())
-        <aside class="sidebar py-8 w-1/4 pr-5">
+        <aside class="sidebar py-8 w-full lg:w-1/4 pr-5">
           @include('partials.sidebar')
         </aside>
       @endif
-      <main class="main py-8 xl:w-3/4 xxl:py-12">
+      <main class="main py-8  w-full xl:w-3/4 xxl:py-12">
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-single-'.get_post_type())
   @endwhile
