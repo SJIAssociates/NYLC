3e74cd627a3df2e89a545ff89861e1951e87ea90
Collin Berg <berg.collin@gmail.com>
2019-11-20 17:23:40 -0500

    Style changes to Events

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -8,14 +8,14 @@
   @include('partials.page-header')
 
   @if(is_tax())
-  <div class='container tax-description'>
-    <div class='text-center lg:w-3/4 xxl:w-1/2 text-lg lg:text-2xl mx-auto mb-10'>
+  <div class='container tax-description mb-8 pb-5'>
+    <div class='text-center lg:w-3/4 xxl:w-1/2 text-lg lg:text-2xl mx-auto'>
       @php echo term_description() @endphp
     </div>
   </div>
   @endif
 
-  <div id="tribe-events-pg-template" class='mb-10'>
+  <div id="tribe-events-pg-template">
     @while(have_posts()) @php the_post() @endphp
 
     @php the_content() @endphp
