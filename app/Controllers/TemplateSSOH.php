ac01f3343c1502ed14cc13d7d94061c14472d2cc
Collin Berg <berg.collin@gmail.com>
2019-12-20 15:38:58 -0500

    Added new views for Flex Generator: Featured Sites Intro News Contact

--- app/Controllers/TemplateSSOH.php
+++ app/Controllers/TemplateSSOH.php
@@ -42,10 +42,11 @@ class TemplateSsoh extends Controller
               'section_title'         => $block['section_title'],
               'right_image'           => $block['right_image'],
               'description'           => $block['description'],
-              'left_link'             => $block['left_link'],
-              'left_link_text'        => $block['left_link_text'],
-              'left_archive_link'     => $block['left_archive_link'],
-              'left_archive_link_text'     => $block['left_archive_link_text']
+              'left_link'             => $block['left_link'] ?? null,
+              'left_link_text'        => $block['left_link_text'] ?? null,
+              'left_archive_link'     => $block['left_archive_link'] ?? null,
+              'left_archive_link_text'     => $block['left_archive_link_text'] ?? null,
+              'background'              => $block['section_background']
             ];
 
             array_push($data, $this_block);
@@ -59,12 +60,45 @@ class TemplateSsoh extends Controller
             ];
 
             array_push($data, $this_block);
+
           } elseif($block['acf_fc_layout'] == 'news') {
 
             $this_block = (object) [
               'index'           => $i,
               'block_type'      => $block['acf_fc_layout'],
               'category'    => $block['category'],
+            ];
+
+            array_push($data, $this_block);
+
+          } elseif($block['acf_fc_layout'] == 'featured_site') {
+
+
+            $featured_blocks = array_map(function ($item) {
+                return [
+                    'title'       => $item->post_title,
+                    'excerpt'     => get_the_excerpt( $item->ID),
+                    'permalink'   => get_the_permalink($item->ID),
+                    'thumbnail'   => get_the_post_thumbnail_url( $item->ID) ?: \App\asset_path('images/placeholder-nylandmarks.png')
+                ];
+            }, $block['sacred_sites']);
+
+
+            $this_block = (object) [
+              'index'        => $i,
+              'block_type'   => $block['acf_fc_layout'],
+              'sites'        => $featured_blocks,
+            ];
+
+            array_push($data, $this_block);
+          } elseif($block['acf_fc_layout'] == 'intro') {
+
+
+            $this_block = (object) [
+              'index'        => $i,
+              'block_type'   => $block['acf_fc_layout'],
+              'intro'        => $block['intro_text'],
+              'background'   => $block['background']
             ];
 
             array_push($data, $this_block);
