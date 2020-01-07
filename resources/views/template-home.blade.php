4245503e18f02c72537c9783f7bced800160cb0d
Collin Berg <berg.collin@gmail.com>
2020-01-07 16:47:29 -0500

    fixed blank link

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -15,7 +15,7 @@
 	<div class='w-11/12 sm:w-2/3 lg:w-1/3 text-center relative'>
 		<h1 class=''>Preserving the City We Love</h1>
 		<p class='text-white mx-auto font-bold mb-5'>We're on the frontlines, protecting the unique landmarks and neighborhoods that make up the heart and soul of New York.</p>
-		<a href="" class='btn text-white my-4 px-12 inline-block'>About Us</a>
+		<a href="/who-we-are" class='btn text-white my-4 px-12 inline-block'>About Us</a>
 	</div>
 </section>
 <section class='full-section' id="homeWhat" aria-label="What We Do Section"  >
