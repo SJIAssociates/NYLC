f621bb468e100ef70979dec0cfaa1cf819da79b0
Collin Berg <berg.collin@gmail.com>
2020-01-16 14:48:22 -0500

    Removed Breadcrumbs from the SSOH Page

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -2,9 +2,11 @@
 <section class='img-bg flex justify-center items-center w-full' style="background-color: #ddd; background-image: url({{ $header_image }}); background-size: cover; background-position: 50% 50%;">
   <h1 class='text-center text-white text-4xl xl:text-5xl xxl:w-1/2'>{!! App::title() !!}</h1>
 </section>
+  @if( get_page_template_slug() != 'views/template-ssoh.blade.php' && !is_singular('tribe_events') )
     <div class="container">
     {!! $breadcrumbs !!}
     </div>
+    @endif
 @else
 
 @if ( is_singular('landmark') )
