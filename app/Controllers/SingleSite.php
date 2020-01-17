f2c58de0d66243b66e4d7394372cb2528b6e8bc6
Collin Berg <berg.collin@gmail.com>
2020-01-17 17:33:42 -0500

    Added Address to next and prev sites

--- app/Controllers/SingleSite.php
+++ app/Controllers/SingleSite.php
@@ -41,10 +41,21 @@ class SingleSite extends Controller
 
     //return $prevPost;
     if($prevPost != ''):
+
+      $location = get_field('location',$prevPost->ID);
+      // Address, City, State zip
+      $Fulladdress = $location['address'];
+
+      $numberAddress = explode(",",$Fulladdress)[0];
+      $city = explode(",",$Fulladdress)[1];
+      $state = explode(",",$Fulladdress)[2];
+
+      $formatAddress = $numberAddress . ",<br />" . $city . ", " . $state;
+
       return (object) array(
         'title'       => $prevPost->post_title,
         'permalink'   => get_the_permalink($prevPost->ID),
-        'excerpt'     => get_the_excerpt($prevPost->ID),
+        'excerpt'     => $formatAddress,
         'thumb'       => get_the_post_thumbnail_url($prevPost->ID,'news_thumb') ?: \App\asset_path('images/placeholder-nylandmarks.png'),
       );
     else:
@@ -58,10 +69,21 @@ class SingleSite extends Controller
     $nextPost =  get_next_post();
 
     if($nextPost != ''):
+
+      $location = get_field('location',$nextPost->ID);
+      // Address, City, State zip
+      $Fulladdress = $location['address'];
+
+      $numberAddress = explode(",",$Fulladdress)[0];
+      $city = explode(",",$Fulladdress)[1];
+      $state = explode(",",$Fulladdress)[2];
+
+      $formatAddress = $numberAddress . ",<br />" . $city . ", " . $state;
+
       return (object) array(
         'title'       => $nextPost->post_title,
         'permalink'   => get_the_permalink($nextPost->ID),
-        'excerpt'     => get_the_excerpt($nextPost->ID),
+        'excerpt'     => $formatAddress,
         'thumb'       => get_the_post_thumbnail_url($nextPost->ID ,'news_thumb') ?: \App\asset_path('images/placeholder-nylandmarks.png'),
       );
     else:
