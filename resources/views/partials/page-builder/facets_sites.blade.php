35728e3eb4daf9af46774587594de65a20b47937
Collin Berg <berg.collin@gmail.com>
2020-01-17 17:37:46 -0500

    Added in the ACF for extra filters

--- resources/views/partials/page-builder/facets_sites.blade.php
+++ resources/views/partials/page-builder/facets_sites.blade.php
@@ -2,16 +2,21 @@
   <div class='container'>
     <div class='flex flex-wrap content'>
       <div class='filters w-full flex flex-wrap border-b-2 border-t-2 border-black mb-5 pt-5 items-end'>
-        <div class='facet-group w-full lg:w-1/4'>
+        <div class='facet-group w-full lg:w-auto pr-5'>
           <h3 class='uppercase font-bold mb-0'>Filter Sites By:</h3>
           @php echo facetwp_display( 'facet', 'location' ) @endphp
         </div>
-        <div class='facet-group w-full lg:w-1/4 facet-neighborhood-header'>
+        <div class='facet-group w-full lg:w-auto facet-neighborhood-header  pr-5'>
           @php echo facetwp_display( 'facet', 'neighborhood' ) @endphp
         </div>
-        <div class='facet-group w-full lg:w-1/2'>
-          @php echo facetwp_display( 'facet', 'activities' ) @endphp
-        </div>
+        @if($block->filters)
+          @foreach ($block->filters as $filter)
+          <div class='facet-group pr-5'>
+            @php echo do_shortcode($filter['filter_shortcode']) @endphp
+          </div>
+          @endforeach
+        @endif
+        <button onclick="FWP.reset()" class='bg-black text-white self-end inline-block border-white border-2 hover:border-black hover:text-black px-10 py-2 mb-6 hover:bg-white lg:ml-auto w-full lg:w-auto'>Reset</button>
       </div>
       <div class='facet_template_area w-full'>
         @php echo facetwp_display( 'template', 'sacred_sites_landing' ) @endphp
