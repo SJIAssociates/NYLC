a59d72e8b5d2c6db8c7a839be07b4ccd4f21398f
Collin B <berg.collin@gmail.com>
2020-01-19 17:19:45 -0500

    Added hover states for header buttons

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,7 @@
           <a class="brand" href="{{ home_url('/') }}" label="Logo for the NYLC. Link to the Homepage">
             <img src="{!! $logo['url'] !!}" alt="Logo for the New York Landmark Conservancy">
           </a>
-        <nav class="hidden lg:block nav-primary">
+        <nav class="nav-primary">
           @if (has_nav_menu('primary_navigation'))
             {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
           @endif
@@ -13,8 +13,8 @@
       </div>
       <div class='w-full lg:w-1/4 lg:block hidden text-right'>
         {!! get_search_form() !!}
-        <a href="/join" class='uppercase text-white text-sm font-bold'>Join Us</a>
-        <a href="/donate" class='btn btn-donate'>Donate</a>
+        <a href="/join" class='uppercase text-white text-sm font-bold hover:bg-white hover:text-primary py-4 px-5'>Join Us</a>
+        <a href="/donate" class='btn btn-donate hover:bg-primary hover:text-white border-2 border-primary hover:border-white'>Donate</a>
       </div>
       <button id='mobile-menu' class='lg:hidden menu-toggle' type='button'>
         <span class='bar top'></span>
