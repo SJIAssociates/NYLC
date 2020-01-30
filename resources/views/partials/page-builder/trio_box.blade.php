c89b6f8ea47001044946ec9438344d0093f0d2b7
Collin Berg <berg.collin@gmail.com>
2020-01-30 12:45:10 -0500

    Added the link to the boxes

--- resources/views/partials/page-builder/trio_box.blade.php
+++ resources/views/partials/page-builder/trio_box.blade.php
@@ -5,7 +5,7 @@
     <div class='w-full lg:w-1/3 text-center items-stretch flex flex-wrap flex-col mb-10'>
       <h3 class='text-3xl'>{!! $triobox['title'] !!}</h3>
       <p class='text-black w-full mx-auto flex-grow my-4 text-xl xl:px-12'>{!! $triobox['description'] !!}</p>
-      <a href="" class='btn text-white  border-red inline-block font-bold w-11/12 xl:w-3/4 items-end mx-auto'>{!! $triobox['buttonText'] !!}</a>
+      <a href="{!! $triobox['button_link'] !!}" class='btn text-white  border-red inline-block font-bold w-11/12 xl:w-3/4 items-end mx-auto'>{!! $triobox['buttonText'] !!}</a>
     </div>
     @endforeach
     </div>
